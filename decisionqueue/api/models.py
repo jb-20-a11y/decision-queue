513fd08483b6047ba1b34b615b0f799858792b69
jb-20-a11y <jalenbprogramerx@gmail.com>
2026-09-05 11:55:36 -0400

    Initial models

--- decisionqueue/api/models.py
+++ decisionqueue/api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class UrgencyChoices(models.IntegerChoices):
+    LOWEST = 1, "Lowest"
+    LOW = 2, "Low"
+    MEDIUM = 3, "Medium"
+    HIGH = 4, "High"
+    HIGHEST = 5, "Highest"
+
+
+class StatusChoices(models.IntegerChoices):
+    PENDING = 1, "Pending"
+    DEFERRED = 2, "Deferred"
+    ACCEPTED = 3, "Accepted"
+    DECLINED = 4, "Declined"
+
+class Item(models.Model):
+    date_created = models.DateTimeField(auto_now=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    problem_statement = models.TextField()
+    urgency = models.IntegerField(choices=UrgencyChoices.choices)
+    status = models.IntegerField(choices=StatusChoices.choices)
+    status_reason = models.TextField()
+
+    class Meta:
+        indexes = [models.Index(fields=["-urgency"])]
+
+
+
